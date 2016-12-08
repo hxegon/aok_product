45ded1867256c3f6fb56dc6ddeae927a68b5a9e5
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-08 10:44:47 -0800

    break out ImageConverter, TransformerSet, IndexGroupable, into different files

---
+++ lib/image_converter.rb
@@ -0,0 +1,55 @@
+# Reformat image fields from jensen csv hashes to strip flattening artifacts.
+# @see .call
+class ImageConverter
+  include IndexGroupable
+
+  # Makes new instance, calls #convert.
+  # @example
+  #   image = { 'image_reference_1' => 'http://www.dot.com',
+  #              'image_name_1'      => 'homepage' }
+  #   ImageConverter.call(image)
+  #   # => image = {
+  #     'url' => 'http://www.dot.com',
+  #     'title' => 'homepage'
+  #   }
+  # @param image [Hash]
+  # @return [Hash]
+  def self.call(image)
+    new(image).send(:convert)
+  end
+
+  # Finds hash fields with a key starting with 'images_'
+  # @param hash [Hash]
+  # @return [Hash]
+  def self.find_images(hash)
+    Hash[hash.select { |(k, _)| k =~ /\Aimage_.+/ }]
+  end
+
+  private
+
+  def initialize(image_hash)
+    @images = image_hash
+  end
+
+  def convert
+    rename_keys!
+    filter_blanks!
+    @images
+  end
+
+  def filter_blanks!
+    is_blank = ->(o) { o.nil? || o.empty? }
+    @images.reject! { |img| is_blank[img['title']] || is_blank[img['url']] }
+  end
+
+  def rename_keys!
+    @images = group_by_index(@images)
+    @images.map! { |g| Hash[g.map { |k, v| [rename_key(k), v] }] }
+  end
+
+  def rename_key(image_key)
+    lookup = { 'reference' => 'url', 'name' => 'title' }
+    lookup[image_key.match(/image_(\w+)_\d+/).captures[0]]
+  end
+end
+
---
+++ lib/transformer_set.rb
@@ -0,0 +1,58 @@
+# INTERFACE: Intended to be called like a proc, or as a kiba transformer with #process
+# EXPECTATIONS: Transformers should return the whole row, not just the part they changed
+# @note There are no guarentees about what order the transformers will be run in.
+class TransformerSet
+  attr_reader :transformers
+
+  def new
+    @transformers = {}
+  end
+
+  # Reduces row with transformers
+  def call(row)
+    blocks.reduce(row) { |row_acc, t| t.call[row_acc] }
+  end
+
+  # @return a proc that calls self.call
+  def to_proc
+    proc { |row| self.call(row) }
+  end
+
+  # @alias for #call
+  # @see for #call
+  def process(row)
+    call(row)
+  end
+
+  # Add a new transformer. Takes a symbol and a block
+  # @params name Symbol
+  # @note if you add a transformer with a name that already exists, the new one
+    # replaces it
+  def add(name, &block)
+    assert name.is_a? Symbol
+    @transformers.merge name: block
+  end
+
+  # remove a transformer by name
+  def remove(name)
+    assert name.is_a? Symbol
+    @transformers.delete(name)
+  end
+
+  # return [Lambdas] returns a list of block type valuse. @transformers.values
+  def blocks
+    @transformers.values
+  end
+
+  # alias for #transformers
+  def to_hash
+    @transformers
+  end
+
+  # Enable block construction syntax. returns self.
+  def generate(&block)
+    yield self
+    self
+  end
+end
+
