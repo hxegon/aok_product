6b1d6b02f6ba60964ba24bc4ea6ed90f485535cb
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-09 14:58:58 -0800

    move default_transformers into CONST, put into source, remove Products::Transform into Source

--- lib/products.rb
+++ lib/products.rb
@@ -6,70 +6,62 @@ require_relative '../image_converter'
 module AOK
   module Products
     class Source
-      attr_reader :products
+      attr_reader :products, :transformers
 
-      def self.from_file(filename, out)
-        new(File.read(filename), out)
-      end
+      # It's not AOK::Product::Source's responsibility to enable transformer extensibility. *POW* TransformerSet *BANG*
+      DEFAULT_TRANSFORMER = TransformerSet.new.generate do |t|
+        t.add(:taxons) do |row|
+        end
 
-      def initialize(text)
-        @products = CSV.parse(text, headers: true, row_sep: :auto)
-      end
+        t.add(:images) do |row|
+          ImageConverter.convert(row)
+        end
 
-      def each
-        @products.each { |p| yield p }
+        t.add(:attributes) do |row|
+          row.tap do |r| # wrap []= so we return the entire hash
+            r['properties'] =
+              r.select { |(header, _)| header[0] == '@' } # find cells with attribute headers
+              .map { |k, v| [k.remove('@'), v] }          # remove attr identifier char: '@'
+              .map { |hash_arr| Hash[hash_arr] }          # convert Hash map output (Array of Arrays) to Array of Hashes
+              .reduce(&:merge)                            # merge Array of Hashes into single hash
+          end
+        end
+
+        t.add(:brand) do |row|
+        end
+
+        t.add(:price) do |row|
+        end
+
+        t.add(:cost) do |row|
+        end
+
+        t.add(:name) do |row|
+        end
+
+        t.add(:description) do |row|
+        end
+
+        t.add(:sku) do |row|
+        end
+
+        t.add(:upc) do |row|
+        end
       end
     end
 
-    class Transform
-      # Needs to be able to turn specific transformers on and off
-      def new(processors = default_processors)
-        @processors = processors
-      end
+    # I don't think this default_transformer thing is working rn :(
+    def self.from_file(transformer=default_transformer, filename)
+      new(transformer, File.read(filename))
+    end
 
-      def [](name)
-        @processors[name.to_sym]
-      end
+    def initialize(transformer=default_transformer, text)
+      @products = CSV.parse(text, headers: true, row_sep: :auto)
+      @transformer = transformer
+    end
 
-      def default_processors
-        # It's not AOK::Product::Source's responsibility to enable transformer extensibility. *POW* TransformerSet *BANG*
-        TransformerSet.new.generate do |t|
-          t.add(:taxons) do |row|
-          end
-
-          t.add(:images) do |row|
-            ImageConverter.convert(row) # TODO: RED
-          end
-
-          t.add(:attributes) do |row|
-          end
-
-          t.add(:brand) do |row|
-          end
-
-          t.add(:price) do |row|
-          end
-
-          t.add(:cost) do |row|
-          end
-
-          t.add(:name) do |row|
-          end
-
-          t.add(:description) do |row|
-          end
-
-          t.add(:sku) do |row|
-          end
-
-          t.add(:upc) do |row|
-          end
-        end
-      end
-
-      def process(row)
-        @processor.call(row)
-      end
+    def each
+      @products.each { |p| yield p }
     end
   end
 end
