ce8919245cfc1b92cc5f27c14f7b6d17a9324e08
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-09 15:00:05 -0800

    stub out product_spec

---
+++ spec/lib/products_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../../lib/products'
+
+Source    = AOK::Products::Source
+Transform = AOK::Products::Transform
+
+RSpec.describe Source do
+  context 'DEFAULT_TRANSFORMER' do
+    context ':taxons' do
+    end
+
+    context ':taxons' do
+    end
+
+    context ':images' do
+    end
+
+    context ':attributes' do
+    end
+
+    context ':brand' do
+    end
+
+    context ':price' do
+    end
+
+    context ':cost' do
+    end
+
+    context ':name' do
+    end
+
+    context ':description' do
+    end
+
+    context ':sku' do
+    end
+
+    context ':upc' do
+    end
+  end
+end
