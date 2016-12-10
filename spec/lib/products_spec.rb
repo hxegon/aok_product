9f96e6d5ff5745b726176bba06345cbae070dd97
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-09 16:08:17 -0800

    change product_spec from unit style to acceptance style.

--- spec/lib/products_spec.rb
+++ spec/lib/products_spec.rb
@@ -4,37 +4,13 @@ Source    = AOK::Products::Source
 
 RSpec.describe Source do
   context 'DEFAULT_TRANSFORMER' do
-    context ':taxons' do
-    end
-
-    context ':taxons' do
-    end
-
-    context ':images' do
-    end
-
-    context ':attributes' do
-    end
-
-    context ':brand' do
-    end
-
-    context ':price' do
-    end
-
-    context ':cost' do
-    end
-
-    context ':name' do
-    end
-
-    context ':description' do
-    end
-
-    context ':sku' do
-    end
-
-    context ':upc' do
+    it 'transforms an example input row to the correct output hash' do
+      # TODO
+      input_row_text =
+        ""
+      expected_product =
+        [{}]
+      expect(Source.new(input_row_text).products).to eq expected_product
     end
   end
 end
