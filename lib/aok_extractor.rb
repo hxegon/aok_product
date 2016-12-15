80052261d75b6a6ff35f39d9f58ed90cb89a66a4
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-15 15:12:31 -0800

    backtest, fix, and finish aok_extractor

--- lib/aok_extractor.rb
+++ lib/aok_extractor.rb
@@ -2,35 +2,36 @@ require_relative 'image_converter'
 require_relative 'extractor'
 
 module AOKExtractor
-  def self.aok_extractor
+  def aok_extractor
     # ... extractor code
-    new.tap do |e|
-      e.add(:taxons) do |row|
-        row.keys.grep(/taxon/i).map do |k|
+    new(:aok).tap do |e|
+      e.add(name: :taxons) do |row|
+        { 'taxons' =>
+          row.keys.grep(/taxon/i).map do |k|
             row[k].split('&&')                                     # Split taxon strings
                   .map { |taxon_string| taxon_string.split('//') } # Split to taxon chain
           end.flatten(1)                                           # turn [ [ [taxon chain] ] ] to [ [taxon chain] ]
+        }
       end
 
-      e.add(:images) do |row|
+      e.add(name: :images) do |row|
         ImageConverter.convert(row)
       end
 
-      e.add(:properties) do |row|
-        row.tap do |r| # wrap []= so we return the entire hash
-          r['properties'] =
-            r.select { |(header, _)| header[0] == '@' } # find cells with attribute headers
-             .map { |k, v| [k.remove('@'), v] }         # remove attr identifier char: '@'
-             .map { |hash_arr| Hash[hash_arr] }         # convert Hash map output (Array of Arrays) to Array of Hashes
+      e.add(name: :properties) do |row|
+        { 'properties' =>
+          row.select { |(header, _)| header[0] == '@' } # find cells with attribute headers
+             .map { |k, v| [k.sub(/@/, ''), v] }        # remove attr identifier char: '@'
+             .map { |hash_arr| Hash[*hash_arr] }        # convert Hash map output (Array of Arrays) to Array of Hashes
              .reduce(&:merge)                           # merge Array of Hashes into single hash
-        end
+        }
       end
 
-      e.add(:brand) do |row|
-        row[row.keys.grep(/brand/i).first]
+      e.add(name: :brand) do |row|
+        { 'brand' => row[row.keys.grep(/brand/i).first] }
       end
 
-      e.add(:price) do |row|
+      e.add(name: :price) do |row|
         # Because this is something that should be:
         # a) Completely unambiguous
         # b) Clear and visible as much as possible
@@ -39,7 +40,7 @@ module AOKExtractor
         raise NotImplementedError, "price extraction should be defined (with #add) by the caller :)"
       end
 
-      e.add(:cost) do |row|
+      e.add(name: :cost) do |row|
         # Because this is something that should be:
         # a) Completely unambiguous
         # b) Clear and visible as much as possible
@@ -48,25 +49,25 @@ module AOKExtractor
         raise NotImplementedError, "cost extraction should be defined (with #add) by the caller :)"
       end
 
-      e.add(:name) do |row|
-        row[row.keys.grep(/title/i).first]
+      e.add(name: :title) do |row| # named :title, rather than :name, because :name is a reserved lookup symbol in Extractor
+        { 'name' => row[row.keys.grep(/title/i).first] }
       end
 
-      e.add(:description) do |row|
-        row[row.keys.grep(/description/i).first]
+      e.add(name: :description) do |row|
+        { 'description' => row[row.keys.grep(/description/i).first] }
       end
 
-      e.add(:sku) do |row|
-        row[row.keys.grep(/sku/i).first]
+      e.add(name: :sku) do |row|
+        { 'sku' => row[row.keys.grep(/sku/i).first] }
       end
 
-      e.add(:upc) do |row|
+      e.add(name: :upc) do |row|
         # Check this expression @ sku
-        row[row.keys.grep(/upc\s*(code)?\s*$/i).first]
+        { 'ucp' => row[row.keys.grep(/upc\s*(code)?\s*$/i).first] }
       end
 
       # Not defined, as specified by the readme
-      # e.add(:options) do |row|
+      # e.add(name: :options) do |row|
       # end
     end
   end
