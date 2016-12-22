2a7b98e1d4de2073e9e7855dac61b08d5b3468e9
Cooper LeBrun <cooperlebrun@gmail.com>
2016-12-22 14:46:57 -0800

    add documentation, remove f2s3 spec stubs

--- lib/s3_destination.rb
+++ lib/s3_destination.rb
@@ -5,33 +5,20 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
-# (expicit/implicit) TODO: The way this class is written doesn't make the difference between:
-# a) A bucket path to tack filenames onto and use as the remote file path
-# and
-# b) A *full*, *explicit* bucket path to be used as a target path, as is.
-
-# (examples) TODO: Provide Example docs with and w/o bucket_name inference.
-#   Should be enough to mitigate the (explicit/implicit) TODO
-
-# (kiba) TODO: Implement any necessary methods for kiba destinations
-
-# BOOKMARK TODO: figure out what env vars need to be present. Document them.
-# should be in a dotenv in a project using s3
-# The .env file, with the auth and required stuff, is on my laptop 8()
-
 # A module that wraps the AWS gem into a focused interface for uploading files
 # to s3. Can infer the bucket path from either the last bucket path you used, or
 # what you specify (either with bucket_path= or passed in with .new).
+# @note You need some env variables with s3 tokens: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION
 # @example Explicit usage pattern:
 #   bucket_name     = 'bucketname'
-#   bucket_path     = ''
-#   local_file_path = ''
+#   bucket_path     = 'Staging/test.json'
+#   local_file_path = 'test.json'
 #   F2S3.upload_file(local_file_path, bucket_path, bucket_name)
 # @example Implicit usage pattern:
-#   bucket_name = ''
-#   bucket_path = ''
+#   bucket_name = 'bucketname'
+#   bucket_path = 'Staging'
 #   client = F2S3.new(bucket_name, bucket_path) # you can also set bucket_path with #bucket_path=
-#   client.upload_file('')
+#   client.upload_file('test.json')
 class F2S3
   # def self.new_from_env
 
@@ -39,6 +26,7 @@ class F2S3
     @s3               = Aws::S3::Resource.new
     @bucket           = @s3.bucket(bucket_name)
     @last_bucket_path = bucket_path
+    @required_keys = %w[AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_REGION].freeze
   end
 
   # def self.upload_file(bucket_name # For a simpler interface
