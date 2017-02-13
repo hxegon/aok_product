89b22828a44cb5192486f819fa7915d735a7794a
Cooper LeBrun <cooperlebrun@gmail.com>
2017-02-13 09:39:10 -0800

    fix ImageConverter test

--- spec/lib/image_converter_spec.rb
+++ spec/lib/image_converter_spec.rb
@@ -5,13 +5,12 @@ RSpec.describe IC do
   context '#call' do
     it 'returns a whole row, with just the images converted' do
       image_urls = %w(http://imgur.com/bbqrofllmao http://imagebucket.com/foobiebelch?=barbazlol)
-      row      = { 'stuff' => 'foo', 'images' => image_urls.join('&&') }
-      expected = { 'stuff' => 'foo', 'images' => [
+      expected = [
         { 'title' => 'Image 1', 'url' => image_urls[0] },
         { 'title' => 'Image 2', 'url' => image_urls[1] }
-      ] }
+      ]
 
-      expect(IC.convert(row)).to eq expected
+      expect(IC.convert(image_urls)).to eq expected
     end
   end
 end
