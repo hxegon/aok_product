ddc35f7ab219ec7e8b9a0053d68054e5183a4f6e
Cooper LeBrun <cooperlebrun@gmail.com>
2017-02-14 08:55:17 -0800

    AOKExtractor#properties: remove parens from block arg, extract regexp to constant

--- lib/aok_extractor.rb
+++ lib/aok_extractor.rb
@@ -15,6 +15,7 @@ end
 class AOKExtractor < Extractor
   using HashGrepFirst
   DEFAULT_IMAGE = 'https://i.imgur.com/BAbXpMz.jpg'.freeze
+  NIL_FIELD_REGEXP = /\A\s*x?\s*\z/i
 
   def taxons(row)
     { 'taxons' =>
@@ -32,11 +33,11 @@ class AOKExtractor < Extractor
 
   def properties(row)
     { 'properties' =>
-      row.select { |(header, _)| header[0] == '@' } # find cells with attribute headers
-        .reject { |_, val| val =~ /\A\s*x?\s*\z/i } # check is only x and or whitespace
-        .map { |(k, v)| [k.sub(/@/, ''), v] }       # remove attr identifier char: '@'
-        .map { |hash_arr| Hash[*hash_arr] }         # convert Hash map output (Array of Arrays) to Array of Hashes
-        .reduce(&:merge) || {}                      # merge Array of Hashes into single hash
+      row.select { |header, _| header[0] == '@' }  # find cells with attribute headers
+        .reject { |_, val| val =~ NIL_FIELD_REGEXP } # check is only x and or whitespace
+        .map { |(k, v)| [k.sub(/@/, ''), v] }        # remove attr identifier char: '@'
+        .map { |hash_arr| Hash[*hash_arr] }          # convert Hash map output (Array of Arrays) to Array of Hashes
+        .reduce(&:merge) || {}                       # merge Array of Hashes into single hash
     }
   end
 
